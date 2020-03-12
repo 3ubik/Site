e46b0d6c73b89cd2d78d7759c0d964460076d5d5
Approxomiron <nzubtsov194@gmail.com>
2020-03-12 16:43:27 +0300

    change

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express =require('express')
 const config=require('config')
 const path = require('path')
 const mongoose=require('mongoose')
+require('dotenv').config()
 
 const app= express()
 
