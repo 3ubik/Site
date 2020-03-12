071abd2e2b531b3edfbfa36d08fbc568b4172d44
Approxomiron <nzubtsov194@gmail.com>
2020-03-12 17:12:48 +0300

    add build

---
+++ client/build/precache-manifest.09ddc62e21a4aa901fcf9d40bf011fa8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3444113fae699507a24191fe6d7c3111",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a230ed31d0df4c0466a",
+    "url": "/static/css/2.41c7419c.chunk.css"
+  },
+  {
+    "revision": "1d61e7e84c9018285f54",
+    "url": "/static/css/main.8169f588.chunk.css"
+  },
+  {
+    "revision": "4a230ed31d0df4c0466a",
+    "url": "/static/js/2.afe1aaa1.chunk.js"
+  },
+  {
+    "revision": "503a35a7784ce5bd12e60cfc0731448a",
+    "url": "/static/js/2.afe1aaa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1d61e7e84c9018285f54",
+    "url": "/static/js/main.a570b01a.chunk.js"
+  },
+  {
+    "revision": "08a01cac7a9424eef63f",
+    "url": "/static/js/runtime-main.ef76e641.js"
+  }
+]);
