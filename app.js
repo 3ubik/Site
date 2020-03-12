1a6f3b9e3fdadbeea928b581b06c55f5e81a9ff8
Approxomiron <nzubtsov194@gmail.com>
2020-03-12 15:45:21 +0300

    change app and index html

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ app.use('/api/auth', require('./routes/auth.routes'))
 app.use('/api/table',require('./routes/table.routes'))
 
 
-if (process.env.NODE_ENV=== 'production') {
+/*if (process.env.NODE_ENV=== 'production') {
     app.use('/',express.static(path.join(__dirname,'client','build')))
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build', 'index.html'))
     })
-}
+}*/
 //const PORT=config.get('port')||5000
 const PORT=process.env.PORT||5000
 
